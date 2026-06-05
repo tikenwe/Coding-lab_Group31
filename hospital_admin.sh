adce6015c131dd396e1aad5d437326c65c6b8e04
rmasugueng-creator <r.masugueng@alustudent.com>
2026-06-05 13:42:32 +0200

    Add secure_data function to enhance security

--- hospital_admin.sh
+++ hospital_admin.sh
@@ -29,3 +29,15 @@ initialize_system() {
 
     echo "System initialization complete."
 }
+# Member 2 - The Security Lead & The Orchestrator
+# secure_data() - Locks down permissions
+secure_data() {
+    echo "Securing active_logs directory..."
+    chmod 700 active_logs
+    echo "Permissions updated:"
+    ls -ld active_logs
+}
+# Execution logic calling The Security Lead & The Orchestrator
+initialize_system
+secure_data
+echo "System Environment Secured - $(date)"

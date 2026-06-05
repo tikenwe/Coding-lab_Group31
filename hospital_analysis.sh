d5e8cfb2e3022f732fa9867039210b2a39f20c45
rmasugueng-creator <r.masugueng@alustudent.com>
2026-06-05 15:26:55 +0200

    Update hospital_analysis.sh

--- hospital_analysis.sh
+++ hospital_analysis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Member 2 - Clinical Analyst & Facility Auditor
+# Member 5 - Clinical Analyst
 # process_vitals() - Extracts critical medical alerts
 process_vitals() {
     echo "Scanning for critical alerts..."
@@ -8,7 +8,7 @@ process_vitals() {
     grep "CRITICAL" active_logs/heart_rate.log active_logs/temperature.log 2>/dev/null | awk -F',' '{print $1, $2, $3}' > reports/critical_alerts.txt
     echo "Critical alerts saved to reports/critical_alerts.txt"
 }
-
+# Member 6 - Facility Auditor
 # water_audit() - Calculates ICU water usage average
 water_audit() {
     echo "Auditing ICU Water Reserve..."
